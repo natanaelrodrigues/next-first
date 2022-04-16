75ddfabada0ecd67fac176549be23b4198cf253d
Natanael R Rodrigues <natanaelrodrigues@gmail.com>
2022-04-15 23:41:30 -0300

    destaque no menu

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,13 +1,22 @@
 import styles from './NavBar.module.css'
 import {navigationLinks} from '../../utils/data';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 
 export const NavBar = () => {
+
+    const router = useRouter();
+
+    router.pathname
+
     return (
         <ul className={styles.container}>
             {navigationLinks.map((link, index)=>(
-                <li key={index} className={styles.linkItem}><Link href={link.path}>{link.label}</Link></li>
+                <li key={index} className={[
+                    styles.linkItem,
+                    router.pathname === link.path ? styles.linkActive : null
+                ].join(" ")}><Link href={link.path}>{link.label}</Link></li>
             ))}
         </ul>
     )
